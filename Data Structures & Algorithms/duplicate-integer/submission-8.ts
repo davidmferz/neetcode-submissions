7a502f8ce76f7da8b55124d559c0e424789b6a91
David Melchor <capricornio_sol1@hotmail.com>
2026-09-01 10:31:08 -0600

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.ts
@@ -0,0 +1,13 @@
+class Solution {
+    hasDuplicate(nums: number[]): boolean {
+       
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] === nums[j]) {
+                    return true; 
+                }
+            }
+        }
+        return false;
+    }
+}
